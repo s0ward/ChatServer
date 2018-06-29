9d30e3188c6ccb0c976c3a5a53d1aff671fa2bb3
s0ward <kristiyantomov@gmail.com>
2018-06-29 08:31:31 +0100

    Update ChatClient.java

--- MultiChat/src/com/company/ChatClient.java
+++ MultiChat/src/com/company/ChatClient.java
@@ -5,7 +5,8 @@ import java.net.Socket;
 public class ChatClient {
 
     public static void main(String[] args) throws Exception {
-        Socket socket = new Socket("78.90.215.146", 9000);
+        //Fill in the IP
+        Socket socket = new Socket("IP-ADDRESS", 9000);
 
     }
 }
